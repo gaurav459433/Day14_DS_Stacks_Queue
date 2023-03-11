735eadac6d0c7f5b2de90146b88d6442efcf0383
gaurav459433 <gaurav459433@gmail.com>
2023-03-12 00:15:54 +0530

    Welcome To Data Structure Stacks & Queue Programs

---
+++ StacksAndQueue.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignmentsrfp259.day14.datastructurestackqueue;
+
+public class StacksAndQueue {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Data Structure Stacks & Queue Programs");
+    }
+}
